3ef430e3f9b8581f26ecf4ffc38e35cca4a870d6
Marc3842h <git@marcsteiner.me>
2018-07-08 22:00:11 +0200

    Begin implementation of buffer class for writing and reading packet contents

---
+++ src/io/osu_buffer.cc
@@ -0,0 +1,94 @@
+#include "osu_buffer.hh"
+
+// Constructor
+shiro::io::buffer::buffer(const std::string &data) {
+
+}
+
+uint8_t shiro::io::buffer::read_byte() {
+    return 0;
+}
+
+void shiro::io::buffer::write_byte(uint8_t value) {
+
+}
+
+int16_t shiro::io::buffer::read_short() {
+    return 0;
+}
+
+void shiro::io::buffer::write_short(int16_t value) {
+
+}
+
+uint16_t shiro::io::buffer::read_ushort() {
+    return 0;
+}
+
+void shiro::io::buffer::write_ushort(uint16_t value) {
+
+}
+
+int32_t shiro::io::buffer::read_int() {
+    return 0;
+}
+
+void shiro::io::buffer::write_int(uint32_t value) {
+
+}
+
+uint32_t shiro::io::buffer::read_uint() {
+    return 0;
+}
+
+void shiro::io::buffer::write_uint(uint32_t value) {
+
+}
+
+int64_t shiro::io::buffer::read_long() {
+    return 0;
+}
+
+void shiro::io::buffer::write_long(int64_t value) {
+
+}
+
+uint64_t shiro::io::buffer::read_ulong() {
+    return 0;
+}
+
+void shiro::io::buffer::write_ulong(uint64_t value) {
+
+}
+
+float shiro::io::buffer::read_float() {
+    return 0;
+}
+
+void shiro::io::buffer::write_float(float value) {
+
+}
+
+double shiro::io::buffer::read_double() {
+    return 0;
+}
+
+void shiro::io::buffer::write_double(double value) {
+
+}
+
+std::string shiro::io::buffer::read_string() {
+    return "";
+}
+
+void shiro::io::buffer::write_string(std::string value) {
+
+}
+
+std::vector<int32_t> shiro::io::buffer::read_array() {
+    return {};
+}
+
+void shiro::io::buffer::write_array(std::vector<int32_t> value) {
+
+}
---
+++ src/io/osu_buffer.hh
@@ -0,0 +1,54 @@
+#ifndef SHIRO_OSU_BUFFER_HH
+#define SHIRO_OSU_BUFFER_HH
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace shiro::io {
+
+    class buffer {
+    private:
+        int position = 0;
+
+    public:
+        buffer(const std::string &data);
+
+        uint8_t read_byte();
+        void write_byte(uint8_t value);
+
+        int16_t read_short();
+        void write_short(int16_t value);
+
+        uint16_t read_ushort();
+        void write_ushort(uint16_t value);
+
+        int32_t read_int();
+        void write_int(uint32_t value);
+
+        uint32_t read_uint();
+        void write_uint(uint32_t value);
+
+        int64_t read_long();
+        void write_long(int64_t value);
+
+        uint64_t read_ulong();
+        void write_ulong(uint64_t value);
+
+        float read_float();
+        void write_float(float value);
+
+        double read_double();
+        void write_double(double value);
+
+        std::string read_string();
+        void write_string(std::string value);
+
+        std::vector<int32_t> read_array();
+        void write_array(std::vector<int32_t> value);
+
+    };
+
+}
+
+#endif //SHIRO_OSU_BUFFER_HH
