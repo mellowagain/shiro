1d5b66f944a0dba31b7f6986e500c6fd53b50598
Marc3842h <git@marcsteiner.me>
2018-07-10 23:22:58 +0200

    Add digestpp as third-party library

---
+++ src/thirdparty/digestpp.hh
@@ -0,0 +1,21 @@
+/*
+This code is written by kerukuro and released into public domain.
+*/
+
+#include "hasher.hh"
+#include "algorithm/sha2.hh"
+#include "algorithm/sha3.hh"
+#include "algorithm/shake.hh"
+#include "algorithm/skein.hh"
+#include "algorithm/blake.hh"
+#include "algorithm/blake2.hh"
+#include "algorithm/groestl.hh"
+#include "algorithm/jh.hh"
+#include "algorithm/md5.hh"
+#include "algorithm/sha1.hh"
+#include "algorithm/sm3.hh"
+#include "algorithm/streebog.hh"
+#include "algorithm/whirlpool.hh"
+#include "algorithm/k12m14.hh"
+#include "algorithm/kupyna.hh"
+#include "algorithm/kmac.hh"
