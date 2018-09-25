524ae5d7e11f9a2697ce17c245031f38d835299b
Marc Steiner <git@marcsteiner.me>
2018-09-25 16:20:45 +0200

    Re-license shiro under AGPL3
    Signed-off-by: Marc3842h <git@marcsteiner.me>
    Signed-off-by: czapek <cyanide.space@gmail.com>

--- src/views/index_page.hh
+++ src/views/index_page.hh
@@ -1,3 +1,21 @@
+/*
+ * shiro - High performance, high quality osu!Bancho C++ re-implementation
+ * Copyright (C) 2018 Marc3842h, czapek
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 unsigned char shiro_index_html[] = {
 0x3c, 0x21, 0x64, 0x6f, 0x63, 0x74, 0x79, 0x70, 0x65, 0x20, 0x68, 0x74,
 0x6d, 0x6c, 0x3e, 0x3c, 0x74, 0x69, 0x74, 0x6c, 0x65, 0x3e, 0x73, 0x68,
