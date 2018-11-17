f1110371e71e8346b26a07244b9279d6a04b93a6
Marc Steiner <git@marcsteiner.me>
2018-11-17 15:48:54 +0100

    Implement API config file with deployment options

---
+++ src/config/api_file.hh
@@ -0,0 +1,34 @@
+/*
+ * shiro - High performance, high quality osu!Bancho C++ re-implementation
+ * Copyright (C) 2018 Marc3842h, czapek
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef SHIRO_API_FILE_HH
+#define SHIRO_API_FILE_HH
+
+#include <string>
+#include <vector>
+
+namespace shiro::config::api {
+
+    extern bool deploy_enabled;
+    extern std::vector<std::string> deploy_keys;
+
+    void parse();
+
+}
+
+#endif //SHIRO_API_FILE_HH
