4b6cbec10858fd6335e15f3c636bc51b9c251ee3
Marc Steiner <git@marcsteiner.me>
2019-03-04 08:56:52 +0100

    Implement osu!direct reverse proxy

---
+++ src/routes/impl/direct/search_set_route.hh
@@ -0,0 +1,30 @@
+/*
+ * shiro - High performance, high quality osu!Bancho C++ re-implementation
+ * Copyright (C) 2018-2019 Marc3842h, czapek
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef SHIRO_SEARCH_SET_ROUTE_HH
+#define SHIRO_SEARCH_SET_ROUTE_HH
+
+#include "../../../thirdparty/crow.hh"
+
+namespace shiro::routes::direct::search_set {
+
+    void handle(const crow::request &request, crow::response &response);
+
+}
+
+#endif //SHIRO_SEARCH_SET_ROUTE_HH
