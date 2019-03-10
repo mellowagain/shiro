6113fe743a4f524f7ecf5048274b448fa9c06e2c
Marc Steiner <git@marcsteiner.me>
2019-03-10 15:24:22 +0100

    Implement multiplayer lobbies (without matches)

---
+++ src/handlers/multiplayer/lobby/lobby_leave_handler.cc
@@ -0,0 +1,27 @@
+/*
+ * shiro - High performance, high quality osu!Bancho C++ re-implementation
+ * Copyright (C) 2018-2019 Marc3842h, czapek
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "../../../multiplayer/lobby_manager.hh"
+#include "lobby_leave_handler.hh"
+
+void shiro::handler::multiplayer::lobby::leave::handle(shiro::io::osu_packet &in, shiro::io::osu_writer &out, std::shared_ptr<shiro::users::user> user) {
+    if (!shiro::multiplayer::lobby_manager::in_lobby(user))
+        return;
+
+    shiro::multiplayer::lobby_manager::remove_user(user);
+}
